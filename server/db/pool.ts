e92044549ce900efd55338fda505e8d39d70dba3
Se Joon Park <sparky9043@gmail.com>
2026-02-14 23:02:40 +0900

    fix: configure database name and add it to pool database property

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -4,6 +4,7 @@ import config from "../src/utils/config";
 const pool = new Pool({
   host: 'localhost',
   user: config.DB_USER,
+  database: config.DB_NAME,
   password: config.DB_PASSWORD,
   port: Number(config.DB_PORT),
 });
