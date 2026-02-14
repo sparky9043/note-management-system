4b768a57927a29812eb057ae1a5f7fe12befb113
Se Joon Park <sparky9043@gmail.com>
2026-02-14 22:51:12 +0900

    chore: update config to get db user, password and port via dotenv

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -3,4 +3,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-export default { PORT };
+const DB_USER = process.env.DB_USER;
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const DB_PORT = process.env.DB_PORT;
+
+export default { PORT, DB_USER, DB_PASSWORD, DB_PORT };
