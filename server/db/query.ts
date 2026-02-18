0f60b1d18a9ea8a6920d2e31c308c5033dc8bc47
Se Joon Park <sparky9043@gmail.com>
2026-02-18 18:43:25 +0900

    chore: add createNote function in query.ts and return NoteEntry

--- server/db/query.ts
+++ server/db/query.ts
@@ -1,4 +1,4 @@
-import { NoteEntry } from "../src/types/notebook";
+import { NewNoteEntry, NoteEntry } from "../src/types/notebook";
 import pool from "./pool";
 
 const getNotes = async (): Promise<NoteEntry[]> => {
@@ -11,4 +11,26 @@ const getNotes = async (): Promise<NoteEntry[]> => {
   return savedNotes.rows;
 };
 
-export default { getNotes };
+const createNote = async (newNoteEntryObject: NewNoteEntry): Promise<NoteEntry> => {
+  const { user_id, section_id, title, content } = newNoteEntryObject;
+
+  if (!user_id || !section_id || !title || !content) {
+    throw new Error('missing fields');
+  }
+
+  const savedNote = await pool.query<NoteEntry>(
+    `
+      INSERT INTO note_entries (user_id, section_id, title, content)
+      VALUES ($1, $2, $3, $4) RETURNING *;
+    `,
+    [Number(user_id), Number(section_id), title, content],
+  );
+
+  if (!savedNote.rows || !savedNote.rows.length) {
+    throw new Error('unexpected error occurred');
+  }
+
+  return savedNote.rows[0];
+};
+
+export default { getNotes, createNote };
