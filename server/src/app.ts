8dcff48d326c57c6b5ef7a2580580e0bbea77fe7
Se Joon Park <sparky9043@gmail.com>
2026-02-18 17:28:41 +0900

    chore: add notes router and initialize in /api/notes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import pingRouter from './router/ping';
+import notesRouter from './router/notes';
 
 const app = express();
 
 app.use('/api/ping', pingRouter);
+app.use('/api/notes', notesRouter);
 
 export default app;
