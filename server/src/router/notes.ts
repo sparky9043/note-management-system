655565262459a4333fc9afa06e1643727b27c235
Se Joon Park <sparky9043@gmail.com>
2026-02-18 18:37:10 +0900

    chore: add GET function that returns notes in db

--- server/src/router/notes.ts
+++ server/src/router/notes.ts
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import query from "../../db/query";
 
 const notesRouter = Router();
+
+notesRouter.get('/', async (_req, res ,next) => {
+  try {
+    const savedNotes = await query.getNotes();
+    res.json(savedNotes);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default notesRouter;
