62e166ec22cc0b0616c051f5d1add304fc233b1e
Se Joon Park <sparky9043@gmail.com>
2026-02-18 18:51:54 +0900

    chore: add POST router in notesRouter for creating new notes

--- server/src/router/notes.ts
+++ server/src/router/notes.ts
@@ -1,5 +1,6 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import query from "../../db/query";
+import { NewNoteEntry, NoteEntry } from "../types/notebook";
 
 const notesRouter = Router();
 
@@ -7,6 +8,15 @@ notesRouter.get('/', async (_req, res ,next) => {
   try {
     const savedNotes = await query.getNotes();
     res.json(savedNotes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+notesRouter.post('/', async (req: Request<unknown, unknown, NewNoteEntry>, res: Response<NoteEntry>, next) => {
+  try {
+    const savedNote = await query.createNote(req.body);
+    res.json(savedNote);
   } catch (error) {
     next(error);
   }
