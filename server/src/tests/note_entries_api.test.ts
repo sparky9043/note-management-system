12b548dcee27f5c0c84f650a79458b88c34557b4
Se Joon Park <sparky9043@gmail.com>
2026-02-20 19:25:01 +0900

    test: reset database and insert default notes before each test

--- server/src/tests/note_entries_api.test.ts
+++ server/src/tests/note_entries_api.test.ts
@@ -1,11 +1,19 @@
-import { describe, test, after } from 'node:test';
+import { describe, test, after, beforeEach } from 'node:test';
 import assert from 'node:assert';
 import pool from '../../db/pool';
 import app from '../app';
 import supertest from 'supertest';
+import note_entries_helper from './note_entries_helper';
 
 const api = supertest(app);
 const baseUrl = '/api/notes';
+
+beforeEach(async () => {
+  await note_entries_helper.resetTable();
+  for (const newNoteEntry of note_entries_helper.defaultNotes) {
+    await note_entries_helper.insertNoteEntry(newNoteEntry);
+  }
+});
 
 void describe('GET Requests', () => {
   void test('/api/notes returns status 200 and all notes', async () => {
