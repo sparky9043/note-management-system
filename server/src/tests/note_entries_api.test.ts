0d08363f8285e3a8755ae82a4008118aa681d6b1
Se Joon Park <sparky9043@gmail.com>
2026-02-20 19:34:42 +0900

    test: return one note for GET request with note id

--- server/src/tests/note_entries_api.test.ts
+++ server/src/tests/note_entries_api.test.ts
@@ -5,6 +5,7 @@ import app from '../app';
 import supertest from 'supertest';
 import note_entries_helper from './note_entries_helper';
 // import { NoteEntry } from '../types/notebook';
+// import { NoteEntry } from '../types/notebook';
 
 const api = supertest(app);
 const baseUrl = '/api/notes';
@@ -26,6 +27,24 @@ void describe('GET Requests', () => {
     assert(Array.isArray(response.body));
 
     assert.strictEqual(note_entries_helper.defaultNotes.length, response.body.length);
+  });
+
+  void test('/api/notes/:id returns status 200 and one note', async () => {
+    const firstNoteEntry = note_entries_helper.defaultNotes[0];
+    const noteEntriesInDb = await note_entries_helper.getNotesInDb();
+
+    const noteEntryId = 1;
+
+    const response = await api
+      .get(`${baseUrl}/${noteEntryId}`)
+      .expect(200);
+    
+    assert.ok(response);
+    assert(typeof response.body === 'object');
+
+    const titles = noteEntriesInDb.map(e => e.title);
+
+    assert(titles.includes(firstNoteEntry.title));
   });
 });
 
