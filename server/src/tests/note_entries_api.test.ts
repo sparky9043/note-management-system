654b7ce2da91cd206a97f737edd1c2f9f0d2ca93
Se Joon Park <sparky9043@gmail.com>
2026-02-20 18:15:24 +0900

    test: expect GET request to /api/notes is okay and body is an array

--- server/src/tests/note_entries_api.test.ts
+++ server/src/tests/note_entries_api.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, after } from 'node:test';
-// import assert from 'node:assert';
+import assert from 'node:assert';
 import pool from '../../db/pool';
 import app from '../app';
 import supertest from 'supertest';
@@ -9,9 +9,12 @@ const baseUrl = '/api/notes';
 
 void describe('GET Requests', () => {
   void test('/api/notes returns status 200 and all notes', async () => {
-    await api
+    const response = await api
       .get(baseUrl)
       .expect(200);
+    
+    assert.ok(response);
+    assert(Array.isArray(response.body));
   });
 });
 
