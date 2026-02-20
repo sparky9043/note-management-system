6637b152c6e2ede94f1a1c03c0170784e37c2491
Se Joon Park <sparky9043@gmail.com>
2026-02-20 18:13:44 +0900

    test: expect status 200 for GET request to /api/notes

--- server/src/tests/note_entries_api.test.ts
+++ server/src/tests/note_entries_api.test.ts
@@ -1,10 +1,17 @@
 import { describe, test, after } from 'node:test';
-import assert from 'node:assert';
+// import assert from 'node:assert';
 import pool from '../../db/pool';
+import app from '../app';
+import supertest from 'supertest';
+
+const api = supertest(app);
+const baseUrl = '/api/notes';
 
 void describe('GET Requests', () => {
-  void test('/api/notes returns status 200 and all notes', () => {
-    assert.strictEqual(1, 1);
+  void test('/api/notes returns status 200 and all notes', async () => {
+    await api
+      .get(baseUrl)
+      .expect(200);
   });
 });
 
