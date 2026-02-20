d33ec17bd1c1d225702821e5a7455f4f9dc6c6e8
Se Joon Park <sparky9043@gmail.com>
2026-02-20 17:54:27 +0900

    fix: remove 2 types and edit NoteEntry type
    I removed Notebook and Section Types. For now, I'm going to just keep
    things simple and keep the title and content properties in each Notes
    and then add more things later

--- server/src/types/notebook.ts
+++ server/src/types/notebook.ts
@@ -1,37 +1,34 @@
 // Types for Notebooks, Sections, and Notes
 
 // Notebook Types
-export interface NewNotebook {
-  user_id: number;
-  name: string;
-}
+// export interface NewNotebook {
+//   user_id: number;
+//   name: string;
+// }
 
-export interface Notebook extends NewNotebook {
-  id: number;
-  created_at: string;
-}
+// export interface Notebook extends NewNotebook {
+//   id: number;
+//   created_at: string;
+// }
 
 // Section Types
-export interface NewSection {
-  user_id: number;
-  notebook_id: number;
-  name: string;
-}
+// export interface NewSection {
+//   user_id: number;
+//   notebook_id: number;
+//   name: string;
+// }
 
-export interface Section extends NewSection {
-  id: number;
-  created_at: string;
-}
+// export interface Section extends NewSection {
+//   id: number;
+//   created_at: string;
+// }
 
 // Entry Types
 export interface NewNoteEntry {
-  user_id: number;
-  section_id: number;
   title: string;
   content: string;
 }
 
 export interface NoteEntry extends NewNoteEntry {
   id: number;
-  created_at: string;
 }
