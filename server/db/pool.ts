0acad61c532cf5da423800911335245167dc70e0
Se Joon Park <sparky9043@gmail.com>
2026-02-23 12:21:09 +0900

    build: update config to change from local to supabase db

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -1,12 +1,21 @@
 import { Pool } from "pg";
 import config from "../src/utils/config";
 
+// X5bzbEk322nt7pjD
+
+// Connection to Local DB
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: config.DB_USER,
+//   database: config.DB_NAME,
+//   password: config.DB_PASSWORD,
+//   port: Number(config.DB_PORT),
+// });
+
+// Connection to Supabase
 const pool = new Pool({
-  host: 'localhost',
-  user: config.DB_USER,
-  database: config.DB_NAME,
-  password: config.DB_PASSWORD,
-  port: Number(config.DB_PORT),
+  connectionString: `postgresql://postgres.hzngyjgzujitgjqprgup:${config.SUPABASE_DB_PASSWORD}@aws-1-ap-south-1.pooler.supabase.com:5432/postgres`
 });
 
 export default pool;
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -11,4 +11,12 @@ const DB_PORT = process.env.DB_PORT || 5432;
 
 const DB_NAME = process.env.DB_NAME;
 
-export default { PORT, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME };
+const SUPABASE_DB_PASSWORD = process.env.SUPABASE_DB_PASSWORD;
+
+export default {
+  PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,DB_NAME,
+  SUPABASE_DB_PASSWORD,
+};
