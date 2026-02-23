c70542c095f07beb7928408656f01c5c63a45075
Se Joon Park <sparky9043@gmail.com>
2026-02-23 12:37:36 +0900

    chore: respond to errors with error code, status and message

--- server/src/utils/middleware.ts
+++ server/src/utils/middleware.ts
@@ -1,7 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import httpErrors from "../errors/httpErrors";
 
-const errorHandler = (error: unknown, _req: Request, _res: Response, next: NextFunction) =>  {
+const errorHandler = (error: unknown, _req: Request, res: Response, next: NextFunction) =>  {
   console.error('inside errorHandler', error);
+
+  if (error instanceof httpErrors.HttpError) {
+    
+    res.status(error.code).json({
+      status: 'error',
+      code: error.code,
+      message: error.message,
+    });
+  };
+  
   next();
 };
 
